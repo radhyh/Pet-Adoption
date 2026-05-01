ed03ed1e87b735a9a23fff61ecbd9c22db5c97d7
niimiyc <miy4h4ni@gmail.com>
2026-05-01 22:16:19 +0800

    Update Dog.java

--- Dog.java
+++ Dog.java
@@ -1,7 +1,27 @@
 public class Dog extends Pet {
     
-    private boolean trained;
+   private boolean trained;
 
     
+    public Dog(String name,int age,String breed, boolean trained ){
     
+       super(name, age, breed);
+       this.trained= trained;
+    
+}
+
+
+@Override
+
+public void sound(){
+    System.out.println("woof!");
+}
+
+public boolean isTrained(){
+    return trained;
+}
+
+public void setTrained(boolean trained){
+    this.trained=trained;
+}
 }
