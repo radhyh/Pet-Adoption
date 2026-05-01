32cb0ee2a4f50f7035ae96c02c52b797cde43ddf
radhyh <nradhiyah.ridzuan@gmail.com>
2026-05-01 22:19:20 +0800

    add dog

--- main.java
+++ main.java
@@ -4,16 +4,13 @@ public class main {
     public static void main(String[] args) {
     
         Cat cat1 = new Cat("Chipsmore", 2, "Siamese", true);
-        //Dog dog1 = new Dog("Oreo", 3, "Chihuahua", true);
+        Dog dog1 = new Dog("Oreo", 3, "Chihuahua", true);
         
         ArrayList<Pet> petList = new ArrayList<>();
         AdoptionCentre centre = new AdoptionCentre("PAC-MAN", petList);
-
+        User user1 = new User("P01",petList);
         
         centre.addPet(cat1);
-
-        //User user1 = new User("P01",petList);
-
         centre.showAvailablePets();
 
         
