8f06d996a7b899f813774873c4f020feeb248ffa
radhyh <nradhiyah.ridzuan@gmail.com>
2026-05-01 21:04:48 +0800

    add adoptPet method

--- AdoptionCentre.java
+++ AdoptionCentre.java
@@ -9,10 +9,30 @@ public class AdoptionCentre {
         this.pets = pets;
         this.centreName = centreName;
     }
+    public void addPet(Pet p){
+        pets.add(p);
+        System.out.println("Welcome " + p.getName() + " to " + centreName + "!");
 
-    public void addPet(Pet pet){
-        pets.add(pet);
-        System.out.println("Welcome " + pet.getName() + " to " + centreName + "!");
+    }
+    public void showAvailablePets(){
+        System.out.println("Current available pets: ");
+        for (Pet p : pets){
 
+            if (p.isAvailable()){
+    
+                p.displayInfo();
+            }
+            else{
+                System.out.println(p.getName() + " is not available.");
+            }
+        }
+
+    }
+    public void adoptPet(User u, Pet p){
+        if(p.isAvailable()){
+            p.setAvailable(false);
+            System.out.println("User ID: " + u.getUserId() + " has adopted " + p.getName());
+            System.out.println("Adoption Sucessful!");
+        }
     }
 }
