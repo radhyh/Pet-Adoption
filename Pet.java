6187e05f278a94c08343713a3243a771a8f88dea
maisara <aliazambrie@gmail.com>
2026-05-01 17:28:16 +0800

    Update Pet.java

--- Pet.java
+++ Pet.java
@@ -1,3 +1,33 @@
-public class Pet {
-    
+public abstract class Pet {
+   
+    private String name;
+    private String breed;
+    private int age;
+    private boolean available = true; 
+
+   
+    public Pet(String name, String breed, int age) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+
+    public abstract void Sound();
+
+    public void displayInfo() {
+        System.out.println("Name: " + name + ", Breed: " + breed + 
+                           ", Age: " + age + ", Available: " + available);
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
