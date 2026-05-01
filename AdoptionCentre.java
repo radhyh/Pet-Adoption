3363a751cc21736c77ffa7c374e5fdb096fd6035
radhyh <nradhiyah.ridzuan@gmail.com>
2026-05-01 17:16:12 +0800

    add main

--- AdoptionCentre.java
+++ AdoptionCentre.java
@@ -1,15 +1,10 @@
 import java.util.ArrayList;
 
 public class AdoptionCentre {
-    public static void main(String[] args) {
     
-        String centreName = "";
-        ArrayList<Pet> pets = new ArrayList<>();
-
-
-
-
-    }
+    String centreName = "";
+    ArrayList<Pet> pets = new ArrayList<>();
+    
     public static void addPet(Pet pet, ArrayList<Pet> pets){
         pets.add(pet);
     }
