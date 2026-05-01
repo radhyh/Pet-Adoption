540cbac997eed1e7ff0f76a588361bb5595a48ea
arfansrna <arfanisrina00@gmail.com>
2026-05-01 22:41:14 +0800

    Add userId field and pet adoption methods

--- User.java
+++ User.java
@@ -1,3 +1,24 @@
 public class User {
     
+    private String userId;
+    Pet adoptedPets;
+
+    public User(String userId){
+        this.userId = userId;
+        
+    public void adoptPet(Pet p){
+        adoptedPet.add(p);
+    }
+    
+    public void returnPet(Pet p){
+        adoptedPet = null;
+    }
+
+    public void setUserId(String userId){
+        this.userId = userId;
+    }
+
+    public String getUserId(){
+        return userId;
+    }
 }
